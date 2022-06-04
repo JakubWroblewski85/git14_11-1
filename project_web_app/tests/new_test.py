86ff55c2f005f17e7cd6aa3f769ff8541338b35a
JakubWroblewski85 <jakub.wroblewski.alk@gmail.com>
2022-06-04 15:07:45 +0200

    upgrade

--- project_web_app/tests/new_test.py
+++ project_web_app/tests/new_test.py
@@ -11,7 +11,7 @@ class NewTest(unittest.TestCase):
 
     def setUp(self):
         # 1. Z otwartą przeglądarką
-        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.Chrome()
         # do jenkinsa
         self.driver = webdriver.Chrome('~/Desktop/git14_11/git14_11-1/project_web_app/chromedriver')
         # self.driver = webdriver.Firefox()
